8b834cde29daef12f27226372efa596374cd0b95
clifford <kpoku823@gmail.com>
2026-03-29 16:01:42 +0100

    Indrotuce Vuln.

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
   res.send('Secure Pipeline Lab Running 🚀');
 });
 
+// 🚨 VULNERABLE ENDPOINT
+app.get('/run', (req, res) => {
+  const userInput = req.query.cmd;
+
+  exec(userInput, (error, stdout, stderr) => {
+    if (error) {
+      return res.send(`Error: ${error.message}`);
+    }
+    res.send(`Output: ${stdout}`);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
