7ead77da64380319e9f8b278fed3c4e805b9f77c
clifford <kpoku823@gmail.com>
2026-03-29 16:53:18 +0100

    update tf

--- iac/main.tf
+++ iac/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_s3_bucket" "my-insecure-bucket-demolab-8877" {
+  bucket = "my-insecure-bucket-demolab-8877"
+}
 
 resource "aws_s3_bucket_public_access_block" "my-insecure-bucket-demolab-8877" {
   bucket = "my-insecure-bucket-demolab-8877"
