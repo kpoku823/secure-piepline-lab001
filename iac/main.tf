f0fb7a6a95de701cf1694443308614a2149a033f
clifford <kpoku823@gmail.com>
2026-03-29 16:49:35 +0100

    tf update block public acls

--- iac/main.tf
+++ iac/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "my-insecure-bucket-demolab-8877" {
+resource "aws_s3_bucket_public_access_block" "my-insecure-bucket-demolab-8877" {
   bucket = "my-insecure-bucket-demolab-8877"
+  block_public_acls = true
 }
